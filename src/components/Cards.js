6e965a1f2ae525b67433ff41c6f6fe13cedc0e2a
Tozarf <Fazatojuni@gmail.com>
2021-07-14 17:13:51 -0500

    Media updates, footer needs adjustments

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,40 +5,40 @@ import CardItem from "./CardItem";
 function Cards() {
   return (
     <div className="cards">
-      <h1>Check out these EPIC Destinations!</h1>
+      <h1>Check out these EPIC collections!</h1>
       <div className="cards__container">
         <div className="cards__wrapper">
           <ul className="cards__items">
             <CardItem
-              src="./../images/img-9.jpg"
-              text="Explore the hidden waterfall deep inside the Amazon Jungle"
-              label="Adventure"
+              src="images/astronaut.jpg"
+              text="My favorite edits"
+              label="Photshop"
               path="/services"
             />
             <CardItem
-              src="images/img-2.jpg"
-              text="Travel through the Islands of Bali in a Private Cruise"
-              label="Luxury"
+              src="images/portrait-1.jpg"
+              text="Check out my portraits porfolio"
+              label="Portraits"
               path="/services"
             />
           </ul>
           <ul className="cards__items">
             <CardItem
               src="images/img-3.jpg"
-              text="Set Sail in the Atlantic Ocean visiting Uncharted Waters"
-              label="Mystery"
+              text="Mexico"
+              label="Landscapes"
               path="/services"
             />
             <CardItem
-              src="images/img-4.jpg"
-              text="Experience Football on Top of the Himilayan Mountains"
-              label="Adventure"
+              src="images/ecuador-1.jpg"
+              text="Ecuador"
+              label="Landscapes"
               path="/products"
             />
             <CardItem
-              src="images/img-8.jpg"
-              text="Ride through the Sahara Desert on a guided camel tour"
-              label="Adrenaline"
+              src="images/bnw-1.jpg"
+              text="My B&W portfolio"
+              label="B&W"
               path="/sign-up"
             />
           </ul>
