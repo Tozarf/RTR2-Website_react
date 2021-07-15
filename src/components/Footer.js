db30ff8aabc51b6e3a8789cb59fd0ed64bbcbd6f
Tozarf <Fazatojuni@gmail.com>
2021-07-14 19:12:18 -0500

    page info(footer) adjustments

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ function Footer() {
     <div className="footer-container">
       <section className="footer-subscription">
         <p className="footer-subscription-heading">
-          Join the Adventure newsletter to receive our best vacation deals
+          Join my newsletter to receive notifications about new posts.
         </p>
         <p className="footer-subscription-text">
           You can unsubscribe at any time.
@@ -30,27 +30,23 @@ function Footer() {
           <div class="footer-link-items">
             <h2>About Us</h2>
             <Link to="/sign-up">How it works</Link>
-            <Link to="/">Testimonials</Link>
-            <Link to="/">Careers</Link>
-            <Link to="/">Investors</Link>
             <Link to="/">Terms of Service</Link>
           </div>
           <div class="footer-link-items">
             <h2>Contact Us</h2>
             <Link to="/">Contact</Link>
             <Link to="/">Support</Link>
-            <Link to="/">Destinations</Link>
             <Link to="/">Sponsorships</Link>
           </div>
         </div>
         <div className="footer-link-wrapper">
-          <div class="footer-link-items">
+          {/* <div class="footer-link-items">
             <h2>Videos</h2>
             <Link to="/">Submit Video</Link>
             <Link to="/">Ambassadors</Link>
             <Link to="/">Agency</Link>
             <Link to="/">Influencer</Link>
-          </div>
+          </div> */}
           <div class="footer-link-items">
             <h2>Social Media</h2>
             <Link to="/">Instagram</Link>
@@ -64,11 +60,10 @@ function Footer() {
         <div class="social-media-wrap">
           <div class="footer-logo">
             <Link to="/" className="social-logo">
-              TRVL
-              <i class="fab fa-typo3" />
+              TOZARF
             </Link>
           </div>
-          <small class="website-rights">TRVL © 2020</small>
+          <small class="website-rights">TOZARF © 2021</small>
           <div class="social-icons">
             <Link
               class="social-icon-link facebook"
